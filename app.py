d340fd6d7dc31cb3801f804c7f7cddfbbba1b703
maradonayan99@gmail.com <maradonayan99@gmail.com>
2024-04-01 15:31:32 -0500

    changes

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
 def index():
     return render_template('single.html')
 
@@ -22,12 +21,14 @@ def serve_app():
     form_data = request.form
     ex_value = form_data.get('ex')
     
+    with open('powset.n', 'r') as file:
+        content = file.read()
     if (ex_value):
         json_data = json.loads(ex_value)
         numbers = stored[0][0]['value']
         vars = stored[0][0]
         final_json,lineno = stored[json_data['exId']]
-        print(stored[0][0])
+        #print('stored data:'+str(stored[0][0]))
         # new_content = final_json["rExercises"][0]['eQuestion'][1]['contents']
         # print(new_content)
         # pattern = r'\d+'
@@ -48,34 +49,45 @@ def serve_app():
         #{"cAction":{"tag":"Check"},"tag":"ExerciseType","exId":0,"exTag":"Powset","cValue":{"roster":"99"}}
         roster_value = json_data['cValue']['roster']
         #print(roster_value)
-        mydata = {'rPages': [], 'rExercises': [], 'rSplash': {'tag': 'SplashPR', 'contents': {'prOutcome': 'POIncorrect', 'prFeedback': [{'tag': 'FText', 'contents': 'In roster notation, '}, {'tag': 'FText', 'contents': 'the powerset 𝒫'}, {'tag': 'FMath', 'contents': cts}, {'tag': 'FText', 'contents': ' is  '}, {'tag': 'FMath', 'contents': temp}, {'tag': 'FText', 'contents': 'Your answer was '}, {'tag': 'FMath', 'contents': str(roster_value)}], 'prTimeToRead': 9}}, 'rSes': '', 'rCurrentPage': None, 'rLogin': None, 'rEcho': None, 'rProgress': None, 'rDone': False}
+        mydata = {'rPages': [], 'rExercises': [], 'rSplash': {'tag': 'SplashPR', 'contents': {'prOutcome': 'POIncorrect', 'prFeedback': [], 'prTimeToRead': 9}}, 'rSes': '', 'rCurrentPage': None, 'rLogin': None, 'rEcho': None, 'rProgress': None, 'rDone': False}
         def get_response():
             return roster_value
+        def fb(txt,val):
+           
+            res2 = ourfunctions.add_to_feedback(txt, val, mydata) 
+            return res2
+        def respond(bool):
+            if bool:
+                mydata['contents']['prOutcome'] = 'POCorrect'
+            return
         vars['get_response'] = get_response
-        vars['give_feedback'] = ourfunctions.give_feedback
-        with open('powset.n', 'r') as file:
-            content = '\n'.join(file.read().split('\n')[lineno:])
+        vars['add_to_feedback'] = fb
+        vars['get_set'] = ourfunctions.get_set
+        vars['get_set2'] = ourfunctions.get_set2
+        vars['respond'] = respond
+        content = '\n'.join(content.split('\n')[lineno:])
+        #print('executing '+str(content))
         exec(content,None,vars)
         return json.dumps(mydata)
     else:
-        with open('powset.n', 'r') as file:
-                content = file.read()
         curjson = {"rPages":[],"rExercises":[{"eTopic":None,"eQuestion":[],"eActions":[{"tag":"Check"}],"eHidden":[{"tag":"FValueS","fvName":"tag","fvValS":"ExerciseType"},{"tag":"FValue","fvName":"exId","fvVal":0},{"tag":"FValueS","fvName":"exTag","fvValS":"Powset"}],"eBroughtBy":[]}],"rSplash":None,"rSes":"","rCurrentPage":None,"rLogin":None,"rEcho":None,"rProgress":None,"rDone":False}
         # {"tag":"FText","contents":"Write the powerset 𝒫"},{"tag":"FText","contents":" in roster notation"},{"tag":"FFieldMath","contents":"roster"}
         vars = {}
         vars['random_unique'] = ourfunctions.random_unique
         def mj(txt,val):
-            print('mjcalled')
+            #print('mjcalled')
             res = ourfunctions.make_json(txt,val,curjson)
             newjson = res
             return res
+        
+
         vars['write_to_exercise'] = mj
         try:
             exec(content, None, vars)
         except NameError as e:
              exc_type, exc_value, exc_traceback = sys.exc_info()
-             line = exc_traceback.tb_lineno
-             print(f'line no {line}')
+             line = exc_traceback.tb_next.tb_lineno-1
+             #print(f'line no {line}')
         curjson["rExercises"][0]['eHidden'] += [{'tag': 'FValue', 'fvName': 'exId', 'fvVal': nextExerciseNr}]
         
         stored[nextExerciseNr] = (vars, line)
--- ourfunctions.py
+++ ourfunctions.py
@@ -1,7 +1,7 @@
 import random
 import json
 def random_unique(minmax, n=2):
-    unique_values = random.sample(range(minmax[0], minmax[1] + 1), 2)
+    unique_values = random.sample(range(minmax[0], minmax[1] + 1), n)
    
     return unique_values
 
@@ -25,7 +25,35 @@ def make_json(updated_tag,values, curr_json):
     
     return curr_json
 
-def get_response(cn):
-    return cn
+def get_response(j):
+    print(j)
+    return 5
+
+def add_to_feedback(updated_tag,values, curr_json):
+    if (type(values) == int):
+        contents_str = str(values)
+    else:
+        contents_str = values
+    data_dict = {"tag": updated_tag, "contents": f"{contents_str}"}
+    curr_json['rSplash']['contents']['prFeedback'].append(data_dict)
+    return curr_json
+
+def get_set(v1, v2):
+    z = r"\left\{\left\{\right\},\left\{value[0]\right\},\left\{value[1]\right\},\left\{value[0],value[1]\right\}\right\}"
+    result_string = z.replace("value[0]", str(v1)).replace("value[1]", str(v2))
+    return result_string
+
+def get_set2(v1, v2):
+    z = r"(\left\{value[0],value[1]\right\})"
+    result_string = z.replace("value[0]", str(v1)).replace("value[1]", str(v2))
+    return result_string
+
+def respond(b):
+    if b:
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
+
 
 
